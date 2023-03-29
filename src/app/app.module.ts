faa7ce609b19f725e0d42581826b27c33e79e0c4
Niharika Rathor <74825076+thisNiharika@users.noreply.github.com>
2023-03-29 18:18:17 +0530

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { AppComponent } from './app.component';
+import { AssessComponent } from './assess/assess.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssessComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/assess/assess.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assess',
+  templateUrl: './assess.component.html',
+  styleUrls: ['./assess.component.css']
+})
+export class AssessComponent {
+
+  
+}
+
+
